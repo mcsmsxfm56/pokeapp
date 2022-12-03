165794a01d2f19be0bc566bfbcb761251bb371e6
mcsmsxfm56 <mcuadrado135@gmail.com>
2022-12-03 14:53:17 -0300

    stablished connection with bd

--- api/server.js
+++ api/server.js
@@ -2,7 +2,10 @@ const express = require('express')
 const server = express()
 require('dotenv').config()
 const { PORT } = process.env
+const sequelize = require('./db.js')
 
-server.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`)
+sequelize.sync({force: true}).then(() => {
+    server.listen(PORT, () => {
+        console.log(`Running on port ${PORT}`)
+    })
 })
