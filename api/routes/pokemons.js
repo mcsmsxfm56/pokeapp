f7ee0f59f3a9fc06fef0865f5538180f19d052ac
mcsmsxfm56 <mcuadrado135@gmail.com>
2022-12-03 18:20:54 -0300

    ruta post terminada

--- api/routes/pokemons.js
+++ api/routes/pokemons.js
@@ -1,12 +1,14 @@
 const { Router } = require('express')
 const router = Router()
+const { Type, Pokemon } = require('../db.js')
+const axios = require('axios')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     let { name } = req.query
     if(name){
         res.status(200).send(`GET /pokemons?name=${name} status 200`)
     } else {
-        res.status(200).send('GET /pokemons status 200')
+        res.status(200).send(`GET /pokemons status 200`)
     }
 })
 
@@ -15,8 +17,23 @@ router.get('/:id', (req, res) => {
     res.status(200).send(`GET /pokemons/${id} status 200`)
 })
 
-router.post('/', (req, res) => {
-    res.status(200).send('POST /pokemons status 200')
+router.post('/', async (req, res) => {
+    let { name, hp, attack, defense, speed, height, weight, types } = req.body
+    let pokemon = await Pokemon.create({
+        name,
+        hp,
+        attack,
+        defense,
+        speed,
+        height,
+        weight
+    })
+
+    types.forEach(async type => {
+        let selectType = await Type.findAll({ where: { name: type } })
+        pokemon.addType(selectType)
+    })
+    res.status(200).send('Pokemon creado con exito')
 })
 
 module.exports = router
